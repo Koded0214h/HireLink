365a8d24c3ce08efeae3cc42d49e7118a8c64e3f
Taofeek Kassim <taophyckassim@gmail.com>
2025-12-18 23:42:49 +0100

    Add Neoon DB connection to sequelize

--- backend/config/sequelize.ts
+++ backend/config/sequelize.ts
@@ -1,17 +1,30 @@
-import { Sequelize } from "sequelize";
-import dotenv from "dotenv";
-dotenv.config();
+// import { Sequelize } from "sequelize";
+// import dotenv from "dotenv";
+// dotenv.config();
 
-const sequelize = new Sequelize(
-  process.env.DATABASE_NAME!,
-  process.env.DATABASE_USER!,
-  process.env.DATABASE_PASSWORD,
-  {
-    host: process.env.DATABASE_HOST,
-    dialect: "postgres",
-    port: Number(process.env.DATABASE_PORT),
-    logging: true,
-  }
-);
+// const sequelize = new Sequelize(
+//   process.env.DATABASE_NAME!,
+//   process.env.DATABASE_USER!,
+//   process.env.DATABASE_PASSWORD,
+//   {
+//     host: process.env.DATABASE_HOST,
+//     dialect: "postgres",
+//     port: Number(process.env.DATABASE_PORT),
+//     logging: true,
+//   }
+// );
+
+import { Sequelize } from "sequelize";
+
+const sequelize = new Sequelize(process.env.DATABASE_URL!, {
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+  logging: false,
+});
 
 export default sequelize;
